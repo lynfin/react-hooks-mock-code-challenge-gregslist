2fc45f586a25e90d8fa04385d1f4119c115e7c03
Lyn <lynfinman@gmail.com>
2022-06-17 08:18:02 -0400

    loaded listings

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  // ┌ └ ─ ├ │
+  // App [searchTerm, listings]
+  // ├ Header
+  // │  └ Search
+  // └ ListingsContainer
+  //    └ ListingCard
+
+  const [listings, setListings] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((r) => r.json())
+      .then((listings) => setListings(listings));
+  }, []);
+
+  console.log(listings);
   return (
     <div className="app">
       <Header />
